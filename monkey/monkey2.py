246c685e0897678f4c26c3319f77d8b7e9c4be87
Edward-Wyatt <37396808+Edward-Wyatt@users.noreply.github.com>
2018-03-15 08:55:43 +0000

    Create monkey2.py

---
+++ monkey/monkey2.py
@@ -0,0 +1,5 @@
+#this one generates a random number corresponding to an ASCII character, checks it against the first character of shakespeare,
+#and if they match, adds it to a text file. it then moves onto the next character, and does the same again until done.
+import random
+def ranChr():
+  1  
